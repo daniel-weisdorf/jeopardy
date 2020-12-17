938ef82be6bb72e8e6e488456f32f206b1bb58af
Daniel Weisdorf <dw1236@gmail.com>
2020-12-17 16:34:37 -0500

    friendly string names + room_code unique constraint

--- jeopardy_backend/jeopardy/models.py
+++ jeopardy_backend/jeopardy/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Game(models.Model):
-    room_code = models.CharField(max_length=10)
+    room_code = models.CharField(max_length=10, unique=True)
     is_complete = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.room_code
 
 class Team(models.Model):
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
@@ -11,15 +14,24 @@ class Team(models.Model):
     score = models.IntegerField(default=0)
     is_picking = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.name
+
 class Player(models.Model):
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     is_captain = models.BooleanField(default=False)
     is_answering = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.name
+
 class Category(models.Model):
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 class Question(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
@@ -28,3 +40,6 @@ class Question(models.Model):
     value = models.IntegerField()
     is_answered = models.BooleanField(default=False)
     is_daily_double = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.question

96300c77170c27b67c6d9333a8f8ae123e762414
Daniel Weisdorf <dw1236@gmail.com>
2020-12-17 17:33:23 -0500

    add viewset + url for games

--- jeopardy_backend/jeopardy/urls.py
+++ jeopardy_backend/jeopardy/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
-
-from . import views
+from django.urls import path, include
+from jeopardy import views
+from rest_framework import routers
+router = routers.DefaultRouter()
+router.register(r'games', views.GameViewset, 'game')
 
 urlpatterns = [
+    path('api/', include(router.urls)),
     path('', views.index, name='index'),
 ]
--- jeopardy_backend/jeopardy/views.py
+++ jeopardy_backend/jeopardy/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+from rest_framework import viewsets
+from jeopardy.serializers import GameSerializer
+from jeopardy.models import Game
 
 def index(request):
     return HttpResponse("Hello, world. You're at the jeopardy index.")
+
+class GameViewset(viewsets.ModelViewSet):
+    serializer_class = GameSerializer
+    queryset = Game.objects.all()
+
+    

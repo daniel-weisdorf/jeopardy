20a40f52d252586689e71f6141f888690a582890
Daniel Weisdorf <dw1236@gmail.com>
2020-12-17 17:45:56 -0500

    base path game because why nest

--- jeopardy_backend/jeopardy_backend/urls.py
+++ jeopardy_backend/jeopardy_backend/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('jeopardy/', include('jeopardy.urls')),
+    path('', include('jeopardy.urls')),
     path('admin/', admin.site.urls),
 ]

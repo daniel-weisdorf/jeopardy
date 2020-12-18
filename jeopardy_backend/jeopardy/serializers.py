f45a388959183166a07bea0b92fefebb4521e8e8
Daniel Weisdorf <dw1236@gmail.com>
2020-12-17 19:12:05 -0500

    host serializer, question doesnt send secret information by default

--- jeopardy_backend/jeopardy/serializers.py
+++ jeopardy_backend/jeopardy/serializers.py
@@ -1,10 +1,26 @@
 from rest_framework import serializers
-from jeopardy.models import Game, Team, Player, Category, Question
+from jeopardy.models import Game, Team, Player, Category, Question, Host
 
 class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Question
+        exclude = [
+            'category',
+            'question',
+            'answer',
+            'is_daily_double',
+            'is_picture_question'
+        ]
+
+class QuestionDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
         exclude = ['category']
+
+class HostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Host
+        exclude = ['game']
 
 class CategorySerializer(serializers.ModelSerializer):
     questions = QuestionSerializer(source='question_set')
@@ -26,6 +42,7 @@ class TeamSerializer(serializers.ModelSerializer):
 class GameSerializer(serializers.ModelSerializer):
     categories = CategorySerializer(source='category_set')
     teams = TeamSerializer(source='team_set')
+    host = HostSerializer(source='host')
     class Meta:
         model = Game
         fields = '__all__'

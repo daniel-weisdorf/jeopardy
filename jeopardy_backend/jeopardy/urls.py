0872a2c2cd85b892abe7b77d332f30c981e149f4
Daniel Weisdorf <dw1236@gmail.com>
2020-12-17 21:38:37 -0500

    other urls from earlier oops

--- jeopardy_backend/jeopardy/urls.py
+++ jeopardy_backend/jeopardy/urls.py
@@ -3,6 +3,8 @@ from jeopardy import views
 from rest_framework import routers
 router = routers.DefaultRouter()
 router.register(r'games', views.GameViewset, 'game')
+router.register(r'teams', views.TeamViewset, 'team')
+router.register(r'players', views.PlayerViewset, 'player')
 
 urlpatterns = [
     path('api/', include(router.urls)),

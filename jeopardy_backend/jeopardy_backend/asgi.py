02f9e23bc95cccfa406645e4134206e80116ccd0
Daniel Weisdorf <dw1236@gmail.com>
2020-12-19 06:21:15 -0500

    base setup for sockets on django side

--- jeopardy_backend/jeopardy_backend/asgi.py
+++ jeopardy_backend/jeopardy_backend/asgi.py
@@ -9,11 +9,13 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+import jeopardy.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jeopardy_backend.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
+	"websocket": URLRouter(jeopardy.routing.websocket_urlpatterns)
 })
--- jeopardy_backend/jeopardy_backend/settings.py
+++ jeopardy_backend/jeopardy_backend/settings.py
@@ -73,6 +73,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'jeopardy_backend.wsgi.application'
 ASGI_APPLICATION = 'jeopardy_backend.asgi.application'
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
